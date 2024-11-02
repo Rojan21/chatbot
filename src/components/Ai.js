95496df563606de79cb0802e1d9efbf1d5d4f4be
ROJAN ROY <87638029+Rojan21@users.noreply.github.com>
2024-11-02 12:42:19 +0000

    finished

--- src/components/Ai.js
+++ src/components/Ai.js
@@ -18,7 +18,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Make sure to include these imports:
 // import { GoogleGenerativeAI } from "@google/generative-ai";
-const genAI = new GoogleGenerativeAI('AIzaSyDxtHHtT3-bnbrKiyCaCXrG8FNp7G1tDa4');
+const genAI = new GoogleGenerativeAI('');
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const chathistory = [
   {
